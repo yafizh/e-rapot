d0fb5565c9a8900a47833727375012b570c26422
A Chairil Yafizh <a.chairil.yafizh@gmail.com>
2022-12-12 23:46:39 +0700

    semester selesai and show up ranking

--- halaman/tampil/kelas_aktif-siswa.php
+++ halaman/tampil/kelas_aktif-siswa.php
@@ -74,7 +74,128 @@ foreach ($semester as $key => $value) {
                 </div>
                 <?php unset($_SESSION['hapus_data']); ?>
             <?php endif; ?>
-            <?php if ($kelas_aktif['status'] == 'Aktif') : ?>
+            <?php if ($kelas_aktif['status'] == 'Selesai') : ?>
+                <div class="col-12">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-md-6">
+                            <div class="card">
+                                <div class="card-header d-flex justify-content-between">
+                                    <h5 class="card-title mb-0 align-self-center">Seluruh Semester</h5>
+                                    <button type="button" class="btn btn-info" data-bs-toggle="modal" data-bs-target="#semesterSelesai">Lihat Peringkat</button>
+                                </div>
+                                <div class="card-body">
+                                    <table class="table table-striped" style="width:100%">
+                                        <thead>
+                                            <tr>
+                                                <th class="text-center td-fit">No</th>
+                                                <th class="text-center">Nama</th>
+                                                <th class="text-center">Status</th>
+                                                <th class="text-center">Rapot</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $query = "
+                                                SELECT 
+                                                    ks.id,
+                                                    ks.status,
+                                                    s.nama 
+                                                FROM 
+                                                    kelas_siswa AS ks 
+                                                INNER JOIN 
+                                                    siswa AS s 
+                                                ON 
+                                                    s.id=ks.id_siswa 
+                                                WHERE 
+                                                    ks.id_kelas_aktif=" . $_GET['id_kelas_aktif'] . " 
+                                                ORDER BY 
+                                                    s.nama 
+                                            ";
+                                            $no = 1;
+                                            $result = $mysqli->query($query);
+                                            ?>
+                                            <?php while ($row = $result->fetch_assoc()) : ?>
+                                                <tr>
+                                                    <td class="text-center td-fit"><?= $no++; ?></td>
+                                                    <td><?= $row['nama']; ?></td>
+                                                    <td class="text-center"><?= $row['status']; ?></td>
+                                                    <td class="text-center td-fit">
+                                                        <?php
+                                                        $q = "
+                                                            SELECT 
+                                                                sk.id,
+                                                                s.nama  
+                                                            FROM 
+                                                                semester_kelas AS sk 
+                                                            INNER JOIN 
+                                                                semester AS s 
+                                                            ON 
+                                                                s.id=sk.id_semester 
+                                                            WHERE 
+                                                                sk.id_kelas_siswa = " . $row['id'] . " 
+                                                            ORDER BY 
+                                                                s.tingkat ASC
+                                                            ";
+                                                        $result2 = $mysqli->query($q);
+                                                        ?>
+                                                        <?php while ($row2 = $result2->fetch_assoc()) : ?>
+                                                            <a href="halaman/cetak/rapot.php?id_semester_kelas=<?= $row2['id']; ?>" class="btn btn-info btn-sm" target="_blank"><?= $row2['nama']; ?></a>
+                                                        <?php endwhile; ?>
+                                                    </td>
+                                                </tr>
+                                            <?php endwhile; ?>
+                                        </tbody>
+                                    </table>
+                                    <div class="modal fade" id="semesterSelesai" tabindex="-1" role="dialog" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title">Seluruh Semester</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                                </div>
+                                                <div class="modal-body m-3">
+                                                    <table class="table table-bordered" style="width:100%">
+                                                        <thead>
+                                                            <tr>
+                                                                <th class="text-center">Nama</th>
+                                                                <th class="text-center td-fit">Ranking</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+                                                            <?php
+                                                            $query = "
+                                                                SELECT 
+                                                                    s.nama 
+                                                                FROM 
+                                                                    kelas_siswa AS ks 
+                                                                INNER JOIN 
+                                                                    siswa AS s 
+                                                                ON 
+                                                                    s.id=ks.id_siswa 
+                                                                WHERE 
+                                                                    ks.id_kelas_aktif=" . $_GET['id_kelas_aktif'] . " 
+                                                                ORDER BY 
+                                                                    s.nama 
+                                                            ";
+                                                            $result = $mysqli->query($query);
+                                                            ?>
+                                                            <?php while ($row = $result->fetch_assoc()) : ?>
+                                                                <tr>
+                                                                    <td><?= $row['nama']; ?></td>
+                                                                    <td></td>
+                                                                </tr>
+                                                            <?php endwhile; ?>
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             <?php endif; ?>
             <?php foreach ($data as $key => $value) : ?>
                 <?php if (!count($value) && $key < (count($data) - 1)) : ?>
@@ -85,7 +206,6 @@ foreach ($semester as $key => $value) {
                         <div class="card-header d-flex justify-content-between">
                             <h5 class="card-title mb-0 align-self-center"><?= $semester[$key]['nama']; ?></h5>
                             <div>
-                                <a href="#" class="btn btn-info">Lihat Peringkat</a>
                                 <?php if ($latest_semester && $kelas_aktif['status'] == 'Aktif') : ?>
                                     <a href="?h=tambah_kelas_aktif-siswa&id_kelas=<?= $_GET['id_kelas'] ?>&id_kelas_aktif=<?= $kelas_aktif['id']; ?>&id_semester=<?= $semester[$key]['id']; ?>" class="btn btn-primary">Tambah Siswa</a>
                                     <a href="?h=tambah_kelas_aktif-semester_kelas&id_kelas=<?= $_GET['id_kelas'] ?>&id_kelas_aktif=<?= $kelas_aktif['id']; ?>&id_semester=<?= $semester[$key]['id']; ?>" class="btn btn-success">Semester Selesai</a>
@@ -94,7 +214,7 @@ foreach ($semester as $key => $value) {
                             </div>
                         </div>
                         <div class="card-body">
-                            <table class="table table-striped datatables-reponsive" style="width:100%">
+                            <table class="table table-striped" style="width:100%">
                                 <thead>
                                     <tr>
                                         <th class="text-center td-fit">No</th>
@@ -107,7 +227,7 @@ foreach ($semester as $key => $value) {
                                     <?php foreach ($value as $row) : ?>
                                         <tr>
                                             <td class="text-center td-fit"><?= $no++; ?></td>
-                                            <td class="text-center"><?= $row['nama']; ?></td>
+                                            <td><?= $row['nama']; ?></td>
                                             <td class="text-center td-fit">
                                                 <a href="?h=tambah_kelas_aktif-siswa-nilai&id_kelas=<?= $_GET['id_kelas'] ?>&id_kelas_aktif=<?= $_GET['id_kelas_aktif'] ?>&id_semester_kelas=<?= $row['id_semester_kelas'] ?>&id_siswa=<?= $row['id_siswa'] ?>&id=<?= $row['id']; ?>" class="btn btn-sm btn-info">Nilai</a>
                                                 <a href="?h=hapus_kelas_aktif-siswa&id_kelas=<?= $_GET['id_kelas'] ?>&id_kelas_aktif=<?= $_GET['id_kelas_aktif'] ?>&id=<?= $row['id']; ?>" class="btn btn-sm btn-danger" onclick="return confirm('Yakin ingin menghapus data ini?')">Hapus</a>
